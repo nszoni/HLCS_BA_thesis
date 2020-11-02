d4578c24aae663bc6cb3a816f794b91c9e185122
Nguyen Nam Son <namson98@gmail.com>
2020-11-02 18:02:50 +0100

    new corrplot with table of significance

--- wave2006.R
+++ wave2006.R
@@ -18,6 +18,9 @@ library(labelled)
 library(sjlabelled)
 library(summarytools)
 library(reshape2)
+library(Hmisc)
+library(corrplot)
+library(caret)
 
 wd <- file.path("~", "thesis_eletpalya", "kesz")
 setwd(wd)
@@ -294,7 +297,17 @@ write.table(divordth, "~/thesis_eletpalya/divordth.txt", sep="\t")
 sepage <- summary(df2[, c("age_at_sepm", "age_at_sepf")])
 write.table(sepage, "~/thesis_eletpalya/sepage.txt", sep="\t")
 
-# Correlation matrices for feature selection----------------------------------------------------
+# Feature selection----------------------------------------------------
+
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
 
 cormatdf <- df2[, c('gender',
                     'mnsal',
@@ -305,59 +318,16 @@ cormatdf <- df2[, c('gender',
                     'nschange',
                     'age_at_sepf')]
 
-cormat <- cormatdf %>% remove_all_labels() %>% cor(use = "complete.obs") %>% round(., 2)
+cor <- cormatdf %>% remove_all_labels() %>% cor(use = "complete.obs") %>% round(., 2)
+res1 <- rcorr(as.matrix(cormatdf))
+res2 <- flattenCorrMatrix(res1$r, res1$P)
 
-# Reorder matrix according to coefficients and hierarchical clustering order
-reorder_cormat <- function(cormat){
-  dd <- as.dist((1-cormat)/2)
-  dd[is.na(dd)] <- 0
-  dd[is.nan(dd)] <- 0
-  hc <- hclust(dd)
-  cormat <-cormat[hc$order, hc$order]
-}
+write.table(res2, file = "~/thesis_eletpalya/res2.txt", sep="\t")
 
-# Get lower triangle of the correlation matrix
-get_lower_tri<-function(cormat){
-  cormat[lower.tri(cormat)] <- NA
-  return(cormat)
-}
+# Insignificant correlation are crossed
+corrplot(res1$r, type="upper", order="hclust", p.mat = res1$P, sig.level = 0.05, tl.col = "black", tl.srt = 45)
+print(findCorrelation(cor, cutoff = 0.5))
 
-cormat <- reorder_cormat(cormat)
-lower_tri <- get_lower_tri(cormat)
-
-# Melt the correlation matrix
-melted_cormat <- melt(lower_tri, na.rm = TRUE)
-
-tiff("test.tiff", units="in", width=5, height=5, res=300)
-# Create a ggheatmap
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value)) +
-  geom_tile(color = "white") +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1) )+
-  coord_fixed()
-
-ggheatmap + 
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                               title.position = "top", title.hjust = 0.5)) +
-  labs(title = "Correlation heatmap of control variables",
-       subtitle = "Observing anomalies in feature selection")
-
-dev.off()
 # Creating panel df -------------------------------------------------------
 
 # pdf <- pdata.frame(df2, index <- c("azon", "hullam")) #cross sectional and wave dimensions
